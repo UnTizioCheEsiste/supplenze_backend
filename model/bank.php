5dfea892a61290f25cec6f63aada85bf0af279a2
Pirraa <pirrafranci2004@gmail.com>
2023-05-04 17:02:50 +0200

    commento query banca ore

--- model/bank.php
+++ model/bank.php
@@ -5,6 +5,7 @@ class Bank extends Database
 {
     public function getArchiveCountHoursBank()
     {
+        //query unica per ore da recuperare e ore straordinari
         $sql = "SELECT u.id, CONCAT(u.nome,' ',u.cognome) as utente, SUM(CASE WHEN bo.tipo_ora = 'da recuperare' THEN bo.numero_ore ELSE 0 END) as ore_da_recuperare, SUM(CASE WHEN bo.tipo_ora = 'straordinario' THEN bo.numero_ore ELSE 0 END) as ore_straordinarie
                 FROM banca_ore bo
                 INNER JOIN utente u ON u.id = bo.docente
