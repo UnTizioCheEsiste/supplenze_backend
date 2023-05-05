98f4a96a1d8b04af164b9d42deede9702b50f2c0
Pirraa <pirrafranci2004@gmail.com>
2023-05-05 20:55:56 +0200

    credenziali raspberry

--- config/config.php
+++ config/config.php
@@ -10,3 +10,10 @@ define("DB_USERNAME", "ukgqgceaavquw");
 define("DB_PASSWORD", "zl690gtougii");
 define("DB_NAME", "dbmuybgxgpjqvi");
 */
+
+/*
+define("DB_HOST", "192.168.1.101");
+define("DB_USERNAME", "francesco");
+define("DB_PASSWORD", "supplenze");
+define("DB_NAME", "dbmuybgxgpjqvi");
+*/
