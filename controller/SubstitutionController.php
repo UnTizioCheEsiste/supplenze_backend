e334370b7f0b7cb3a90373c09e68ae57720d0b8d
xclaree <valeriabaleanu@gmail.com>
2023-05-05 09:20:31 +0200

    substitute, inizio sviluppo api disponibilita

--- controller/SubstitutionController.php
+++ controller/SubstitutionController.php
@@ -19,61 +19,72 @@ class SubstitutionController extends BaseController
             case "addSubstitute":
                 $json = file_get_contents('php://input');
                 $data = json_decode($json);
-                //$id_absence, $id_user, $not_necessary, $to_pay
-                //VARIABILI NECESSARIE
+                //VARIABILI NECESSARIE: $id_absence, $id_user, $not_necessary, $to_pay
+                if (empty($data->assenza) || empty($data->supplente) || empty($data->ora) || !is_int($data->da_retribuire) || !is_int($data->non_necessaria)) {
+                    //errore perche sono variabili necessarie
+                    http_response_code(500);
+                    echo json_encode(["success" => false, "data" => "Non sono presenti tutti gli attributi"]);
+                    break;
+                }
                 $id_absence = $data->assenza;
                 $id_user = $data->supplente;
                 $not_necessary = $data->non_necessaria;
                 $to_pay = $data->da_retribuire;
                 $hour = $data->ora;
-                if(empty($id_absence) || empty($id_user) || empty($not_necessary) || empty($to_pay) || empty($hour)){
-                    //errore perche sono variabili necessarie 
-                    http_response_code(500);
-                    echo json_encode(["success" => false, "data" => "Le variabili necessarie non sono presenti"]);
-                    break;
-                }
+
                 //VARIABILI NON NECESSARIE
                 $substitution_date = $data->data_supplenza;
                 $note = $data->nota;
+
+                if (empty($substitution_date) || empty($note)) { //se sono null le devo convertire in stringhe vuote
+                    $substitution_date = "";
+                    $note = "";
+
+                }
                 $newSubstitute = $sub->addSubstitute($id_absence, $id_user, $not_necessary, $to_pay, $hour, $substitution_date, $note);
 
-                if(!$newSubstitute){ //se ritorna FALSE
+                if (!$newSubstitute) { //se ritorna FALSE
                     http_response_code(500);
                     echo json_encode(["success" => false, "data" => "Operazione non completata"]);
-                    break; 
+                    break;
                 }
                 http_response_code(200);
-                echo json_encode(["success" => true, "data" => "riga aggiunta con successo"]); 
+                echo json_encode(["success" => true, "data" => "Riga aggiunta con successo"]);
                 break;
             // case "addSubtituteTeaching":
             //     break;
             case "getArchiveSubstitution":
                 $archiveSub = $sub->getArchiveSubstitution();
-                if(empty($archiveSub)){
+                if (empty($archiveSub)) {
                     http_response_code(500);
                     echo json_encode(["success" => false, "data" => "Errore nell'operazione"]);
                     break;
                 }
                 http_response_code(200);
-                echo json_encode(["success" => true, "data" => $archiveSub]); 
+                echo json_encode(["success" => true, "data" => $archiveSub]);
                 break;
             case "getArchiveUserSubstitution":
                 $params = $this->getQueryStringParams();
-                if(!empty($params["id"]))
-                {
+                if (empty($params["id"])) { //se non è stato indicato l'ID
+                    http_response_code(200);
+                    echo json_encode(["success" => false, "data" => "Non è presente l'id"]);
+                    break;
+                } else if(is_int($params["id"])){
                     $archiveUserSub = $sub->getArchiveUserSubstitution($params['id']);
-                    if(empty($archiveUserSub)){
-                        http_response_code(500);
+                    if (empty($archiveUserSub)) {
+                        http_response_code(200);
                         echo json_encode(["success" => false, "data" => "L'utente non ha supplenze"]);
                         break;
                     }
                     http_response_code(200);
-                    echo json_encode(["success" => true, "data" => $archiveUserSub]); 
+                    echo json_encode(["success" => true, "data" => $archiveUserSub]);
                     break;
-                }else{
-                    echo json_encode(["success" => false, "data" => "Errore nell'operazione"]);
+                } else {
+                    http_response_code(200);
+                        echo json_encode(["success" => false, "data" => "L'id inserito non è un intero"]);
+                        break;
                 }
-                
+
         }
     }
 }
--- model/availability.php
+++ model/availability.php
@@ -3,5 +3,43 @@ require_once PROJECT_ROOT_PATH . "/model/database.php";
 
 class Availability extends Database
 {
-    
+    //getArchiveAvailability, getArchiveAvailabilityHour, addAvailability, 
+    //removeAvailability, getArchiveTypeAvailability 
+    public function getArchiveAvailability($date)
+    { //Mostra la lista dei supplenti disponibili per quel determinato giorno
+        // Da controllare la presenza del possibile supplente
+        //data inizio e data fine possono essere sia date normali (data+ora) oppure giorni (lunedi, martedi) 
+        // e si devono contraddistinguere con due diverse query
+
+        $sql1 = "SELECT d.id, u.id as id_utente, CONCAT(u.nome, ' ', u.cognome) as supplente, td.nome as tipo_disponibilita, d.data_inizio, d.data_fine 
+                from utente u
+                inner join disponibilita d
+                on u.id = d.docente
+                inner join tipo_disponibilita td 
+                on td.id = d.tipo_disponibilita
+                where date(d.data_inizio) = :date_hour";
+        $sql2 = "SELECT d.id, u.id as id_utente, CONCAT(u.nome, ' ', u.cognome) as supplente, td.nome as tipo_disponibilita, d.giorno, d.ora
+                from utente u
+                inner join disponibilita d
+                on u.id = d.docente
+                inner join tipo_disponibilita td 
+                on td.id = d.tipo_disponibilita
+                inner join giorno g 
+                on g.id = d.giorno 
+                inner join ora o 
+                on o.id = d.ora 
+                where g.nome = :week_day";
+
+        if (DateTime::createFromFormat('Y-m-d H:i:s', $date) !== false) {
+            $stmt = $this->conn->prepare($sql1);
+            $stmt->bindValue(":date_hour", $date, PDO::PARAM_STR);
+        } else {
+            $stmt = $this->conn->prepare($sql2);
+            $stmt->bindValue(":week_day", $date, PDO::PARAM_STR);
+
+        }
+    }
+
+
+
 }
