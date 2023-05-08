78375ad094991957eb051da53c33c36279e1a4f3
UnTizioCheEsiste <nicolociancaglia@gmail.com>
2023-05-08 18:07:17 +0200

    WIP Absence + Absence Controller

--- controller/AbsenceController.php
+++ controller/AbsenceController.php
@@ -17,20 +17,45 @@ class AbsenceController extends BaseController
 
         switch ($this->uri) {
             case "getArchiveAbsence":
+                // (5)
                 $absence->getArchiveAbsence();
                 break;
             case "getAbsence":
+                // (6)
                 break;
             case "addCertificate":
+                // (7)
                 break;
             case "addAbsenceHour":
+                // (1)
                 break;
             case "addAbsenceSingleDay":
+                // (2)
                 break;
             case "addAbsenceMultipleDay":
+                // (3)
                 break;
             case "ungroupAbsence":
+                // (4)
+                $params = $this->getQueryStringParams();
+                if(empty($params["id"]))
+                {
+                    http_response_code(404);
+                    echo json_encode(["success" => false, "data" => "Assenza non trovata"]);
+                    break;
+                }else{
+                    $success = $absence->ungroupAbsence($params['id']);
+
+                if (empty($success)) {
+                    http_response_code(404);
+                    echo json_encode(["success" => false, "data" => "Assenza non trovata"]);
+                    break;
+                }
+
+                http_response_code(200);
+                echo json_encode(["success" => true, "data" => $success]);
                 break;
+                }
         }
     }
 }
--- model/absence.php
+++ model/absence.php
@@ -1,5 +1,6 @@
 <?php
 require_once PROJECT_ROOT_PATH . "/model/database.php";
+require_once PROJECT_ROOT_PATH . "/model/time.php";
 
 class Absence extends Database
 {
@@ -29,6 +30,71 @@ class Absence extends Database
 
         // Array associativo con tutte le informazioni necessarie
         $users = array();
+    }
+
+    /**
+     * Divide l'assenza in supplenze da coprire.
+     * 
+     * @param int id ID dell'assenza.
+     * @return mixed supplenze singole e giorno.
+     */
+    public function ungroupAbsence($id) 
+    {
+        // Get dell'assenza
+        $sql = "SELECT *
+                FROM assenza
+                WHERE id = :id";
+        
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $absence = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // Data inizio
+        $di = new DateTime($absence["data_inizio"]);
+
+        // Data fine
+        $df = new DateTime($absence["data_fine"]);
+
+        $dateDifference = date_diff($df, $di);
+
+        // Divisione data da ora
+        $data_inizio = explode(" ", $absence["data_inizio"]);
+        $data_fine = explode(" ", $absence["data_fine"]);
+
+        $time = new Time();
+        $hours = $time->getHour();
+
+        // Se l'assenza rientra in un giorno
+        if ($data_inizio[0] === $data_fine[0]) 
+        {
+            $hourId = 0;
+            // Get dell'id dell'ora
+            foreach ($hours as $hour) 
+            {
+                // Se l'ora di inizio e di fine combaciano con un'ora della tabella "ora"
+                if ($hour["data_inizio"] === $data_inizio[1] && $hour["data_fine"] === $data_fine[1]) 
+                {
+                    // hourId prende l'id di quell'ora
+                    $hourId = $hour["id"];
+                    break;
+                }
+            }
+
+            // Insert della supplenza singola
+            $sql = "INSERT INTO supplenza (assenza, ora, data_supplenza)
+                    VALUES (:assenza, :ora, :data_supplenza)";
+                
+            $stmt = $this->conn->prepare($sql);
+            $stmt->bindValue(':assenza', $id, PDO::PARAM_INT);
+            $stmt->bindValue(':ora', $hourId, PDO::PARAM_STR);
+            $stmt->bindValue(':data_supplenza', $data_inizio[0], PDO::PARAM_STR);
+            $stmt->execute();
+        }
+        else 
+        {/* Assenza giorni multipli */}
+
     }
 }
 
