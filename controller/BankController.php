616f22d63d82f565e5630d6da08e84fba46d1322
UnTizioCheEsiste <nicolociancaglia@gmail.com>
2023-06-07 18:23:11 +0200

    Banca ore finita

--- controller/BankController.php
+++ controller/BankController.php
@@ -18,7 +18,7 @@ class BankController extends BaseController
         switch ($this->uri) {
             case "getArchiveCountHoursBank":
                 $result = $bank->getArchiveCountHoursBank();
-                
+
                 if (empty($result)) {
                     http_response_code(400);
                     echo json_encode(["success" => false, "data" => "Errore nel download dei dati"]);
@@ -30,28 +30,26 @@ class BankController extends BaseController
                 break;
             case "getUserHoursBank":
                 $params = $this->getQueryStringParams();
-                if(!empty($params["id"]))
-                {
+                if (!empty($params["id"])) {
                     $result = $bank->getUserHoursBank($params['id']);
-    
+
                     http_response_code(200);
                     echo json_encode(["success" => true, "data" => $result]);
                     break;
-                }else{
+                } else {
                     http_response_code(404);
                     echo json_encode(["success" => false, "data" => "Id non inserito."]);
                     break;
                 }
             case "getUserCountHoursBank":
                 $params = $this->getQueryStringParams();
-                if(!empty($params["id"]))
-                {
+                if (!empty($params["id"])) {
                     $result = $bank->getUserCountHoursBank($params['id']);
-    
+
                     http_response_code(200);
                     echo json_encode(["success" => true, "data" => $result]);
                     break;
-                }else{
+                } else {
                     http_response_code(404);
                     echo json_encode(["success" => false, "data" => "Id non inserito."]);
                     break;
@@ -59,28 +57,39 @@ class BankController extends BaseController
             case "addUserHoursBank":
                 $json = file_get_contents('php://input');
                 $data = json_decode($json);
-                
-                if(empty($data->utente) || empty($data->tipo) | empty($data->numero_ore))
-                {
+
+                if (empty($data->utente) || empty($data->tipo) | empty($data->numero_ore)) {
                     http_response_code(400);
                     echo json_encode(["success" => false, "data" => "Non sono stati inseriti tutti i parametri"]);
                     break;
                 }
 
-                if(empty($data->nota)){
+                if (empty($data->nota)) {
                     $notes = " ";
                 } else {
                     $notes = $data->nota;
                 }
 
-                if($bank->addUserHoursBank($data->utente, $data->tipo, $data->numero_ore, $notes) !=0)
-                {
+                if ($bank->addUserHoursBank($data->utente, $data->tipo, $data->numero_ore, $notes) != 0) {
                     http_response_code(200);
                     echo json_encode(["success" => true, "data" => "Ore aggiunte correttamente"]);
-                }else{
+                } else {
                     http_response_code(500);
                     echo json_encode(["success" => false, "data" => "Errore nell'inserimento"]);
                 }
+                break;
+            case "removeHours":
+                $params = $this->getQueryStringParams();
+
+                if (!$params["id"]) {
+                    http_response_code(400);
+                    echo json_encode(["success" => false, "data" => "Non sono stati inseriti tutti i parametri"]);
+                    break;
+                }
+
+                $result = $bank->removeHours($params["id"]) > 0 ? true : false;
+                $result ? http_response_code(200) : http_response_code(500);
+                echo json_encode(["success" => $result, "data" => $result]);
                 break;
             default:
                 http_response_code(400);
--- model/bank.php
+++ model/bank.php
@@ -4,10 +4,10 @@ require_once PROJECT_ROOT_PATH . "/model/database.php";
 class Bank extends Database
 {
     /**
-    * Restituisce lo storico delle ore da recuperare o straordinarie.
-    *
-    * @return mixed Array di ore.
-    */
+     * Restituisce lo storico delle ore da recuperare o straordinarie.
+     *
+     * @return mixed Array di ore.
+     */
     public function getArchiveCountHoursBank()
     {
         $sql = "SELECT u.id, CONCAT(u.nome,' ',u.cognome) as utente, SUM(CASE WHEN bo.tipo_ora = 'da recuperare' THEN bo.numero_ore ELSE 0 END) as ore_da_recuperare, SUM(CASE WHEN bo.tipo_ora = 'straordinario' THEN bo.numero_ore ELSE 0 END) as ore_straordinarie
@@ -23,12 +23,12 @@ class Bank extends Database
     }
 
     /**
-    * Restituisce tutte le ore da recuperare o straordinarie di un utente.
-    *
-    * @param int $id ID dell'utente.
-    *
-    * @return mixed Array di ore.
-    */
+     * Restituisce tutte le ore da recuperare o straordinarie di un utente.
+     *
+     * @param int $id ID dell'utente.
+     *
+     * @return mixed Array di ore.
+     */
     public function getUserHoursBank($id)
     {
         $sql = "SELECT bo.docente, bo.numero_ore, bo.nota, bo.tipo_ora 
@@ -43,12 +43,12 @@ class Bank extends Database
     }
 
     /**
-    * Restituisce il totale di ore da recuperare o straordinarie.
-    *
-    * @param int $id ID dell'utente.
-    *
-    * @return mixed Array di ore.
-    */
+     * Restituisce il totale di ore da recuperare o straordinarie.
+     *
+     * @param int $id ID dell'utente.
+     *
+     * @return mixed Array di ore.
+     */
     public function getUserCountHoursBank($id)
     {
         $sql = "SELECT u.id, CONCAT(u.nome,' ',u.cognome) as utente, SUM(CASE WHEN bo.tipo_ora = 'da recuperare' THEN bo.numero_ore ELSE 0 END) as ore_da_recuperare, SUM(CASE WHEN bo.tipo_ora = 'straordinario' THEN bo.numero_ore ELSE 0 END) as ore_straordinarie
@@ -65,21 +65,21 @@ class Bank extends Database
     }
 
     /**
-    * Aggiunge un'ora alla banca d'ore di un utente.
-    *
-    * @param int $userId ID dell'utente.
-    * @param string $day Giorno.
-    * @param string $type Da recuperare o straordinario.
-    * @param int $count Il totale di ore da aggiungere.
-    * @param string $notes Note.
-    *
-    * @return boolean True se l'aggiunta è andata a buon fine.
-    */
+     * Aggiunge un'ora alla banca d'ore di un utente.
+     *
+     * @param int $userId ID dell'utente.
+     * @param string $day Giorno.
+     * @param string $type Da recuperare o straordinario.
+     * @param int $count Il totale di ore da aggiungere.
+     * @param string $notes Note.
+     *
+     * @return boolean True se l'aggiunta è andata a buon fine.
+     */
     public function addUserHoursBank($userId, $type, $count, $notes)
     {
         $sql = "INSERT into banca_ore  (docente, tipo_ora, numero_ore, nota)
                 values (:userId, :type, :count, :notes);";
-       
+
         $stmt = $this->conn->prepare($sql);
         $stmt->bindValue(":userId", $userId, PDO::PARAM_INT);
         $stmt->bindValue(":type", $type, PDO::PARAM_STR);
@@ -87,5 +87,25 @@ class Bank extends Database
         $stmt->bindValue(":notes", $notes, PDO::PARAM_STR);
 
         return $stmt->execute();
+    }
+
+    /**
+     * Cancella l'insieme delle ore identificate dall'id della banca ore
+     * 
+     * @param int $id ID della riga nella banca ore
+     * 
+     * @return int Numero di righe cancellate (se tutto va a buon fine dovrebbe essere 1)
+     */
+    public function removeHours($id)
+    {
+        $sql = "DELETE
+                FROM banca_ore
+                WHERE id = :id";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $result = $stmt->execute();
+
+        return $stmt->rowCount();
     }
 }
